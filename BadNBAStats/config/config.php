33cc6e137fe7fefce74837d6cb0d1bc44d2b9045
raylandho <89878335+raylandho@users.noreply.github.com>
2022-08-18 15:48:13 -0700

    Create config.php

---
+++ BadNBAStats/config/config.php
@@ -0,0 +1,10 @@
+<?php
+// Most files import config.php so going to start the session here. This means most pages will get the session started.
+session_start();
+
+define('DB_HOST', '303.itpwebdev.com');
+define('DB_USER', 'rsho_db_user');
+define('DB_PASS', 'junk4college');
+define('DB_NAME', 'rsho_nbamvp');
+
+?>
